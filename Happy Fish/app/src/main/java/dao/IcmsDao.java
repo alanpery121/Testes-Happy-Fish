47ab5a8207010dee010d382f62e9eb30379c8583
victorlima395 <victorlima395@gmail.com>
2020-05-09 19:29:04 -0300

    Agora sim. Finalizado com suceso

--- Happy Fish/app/src/main/java/dao/IcmsDao.java
+++ Happy Fish/app/src/main/java/dao/IcmsDao.java
@@ -5,7 +5,7 @@ import model.Icms;
 public class IcmsDao {
 
     public boolean salvar(Icms icms){
-        System.out.println("Icms salvo com sucesso"+"\nO estado "+icms.getEstado()+" tem uma aliquota de "+icms.getAliquota()+"%");
+        System.out.println("Icms salvo com sucesso"+"\nO estado "+icms.getEstado()+" tem uma aliquota de: "+icms.getAliquota()+"%");
         return  true;
     }
 
